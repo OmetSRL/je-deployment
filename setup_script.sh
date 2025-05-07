e94340aad4ba6db2f42265a28282ad0271d7030f
Francesco Orsenigo <francesco.orsenigo@omet.it>
2025-05-07 12:18:58 +0200

    removed the pull of the images from the ACR since it's not needed

--- setup_script.sh
+++ setup_script.sh
@@ -5,8 +5,8 @@ set -e
 
 # === CONFIGURATION ===
 SHARE_NAME="csv_import"
-SHARE_PATH="/srv/samba/$SHARE_NAME"
-WINDOWS_GROUP_NAME="sambasharegrp"
+SHARE_PATH="/home/shares/$SHARE_NAME"
+WINDOWS_GROUP_NAME="windowsgroup"
 DEST_DIR="$SHARE_PATH/"
 
 # Azure service principal credentials
@@ -85,22 +85,14 @@ dns proxy = no
         sudo systemctl restart smbd
 fi
 
-# login into the service principal, then into ACR and pull all the latest images
+# login into the service principal then into ACR 
 echo "=== Logging in with service principal ==="
 sudo az login --service-principal \
     --username "$AZ_CLIENT_ID" \
     --password "$AZ_CLIENT_SECRET" \
     --tenant "$AZ_TENANT_ID"
 
-echo "Logging into Azure Container Registry and pulling 'latest' images..."
+echo "Logging into Azure Container Registry"
 sudo az acr login --name "$AZURE_ACR_NAME"
 
-REPOS=$(sudo az acr repository list --name "$AZURE_ACR_NAME" --output tsv)
-
-for REPO in $REPOS; do
-    FULL_IMAGE="$AZURE_ACR_NAME.azurecr.io/$REPO:latest"
-    echo "Pulling $FULL_IMAGE"
-    sudo docker pull "$FULL_IMAGE"
-done
-
-echo "Setup complete! Samba shared folder configured at $SHARE_PATH and images pulled"
+echo "Setup complete! Samba shared folder configured at $SHARE_PATH and logged in ACR"
